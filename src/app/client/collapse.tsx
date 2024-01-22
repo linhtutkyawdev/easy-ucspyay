aec92bb192e3ccc9316c7133143322cf6f6511e7
linhtutkyaw <linhtutkyaw.dev@gmail.com>
2024-01-23 03:58:41 +0630

    wev

--- src/app/client/collapse.tsx
+++ src/app/client/collapse.tsx
@@ -1,19 +1,12 @@
-import { QrCodeIcon } from "@heroicons/react/24/solid";
+import { QrCodeIcon, UserIcon } from "@heroicons/react/24/solid";
 import {
   Collapse as Clp,
   Typography,
   Progress,
   Button,
 } from "@material-tailwind/react";
-import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { useAppSelector } from "@/lib/hooks";
 
-const NAV_MENU = [
-  {
-    name: "Share QR code",
-    icon: QrCodeIcon,
-    href: "verify",
-  },
-];
 interface NavItemProps {
   children: React.ReactNode;
   href?: string;
@@ -38,9 +31,43 @@ const Collapse = ({ open }: { open: boolean }) => {
   const events = useAppSelector((state) => state.event.events);
   const user = useAppSelector((state) => state.user.user);
   const votesLeft = useAppSelector((state) => state.user.votesLeft);
-
-  const dispatch = useAppDispatch();
-
+  let NAV_MENU = [
+    {
+      name: "Share QR code",
+      icon: QrCodeIcon,
+      href: "verify",
+    },
+    {
+      name: "Scan QR",
+      icon: () => (
+        <svg
+          fill="currentColor"
+          stroke-width="0"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          height="1.25rem"
+          width="1.25rem"
+          style={{ overflow: "visible", color: "currentcolor" }}
+        >
+          <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0v-3Zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5ZM.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5Zm15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5ZM4 4h1v1H4V4Z"></path>
+          <path d="M7 2H2v5h5V2ZM3 3h3v3H3V3Zm2 8H4v1h1v-1Z"></path>
+          <path d="M7 9H2v5h5V9Zm-4 1h3v3H3v-3Zm8-6h1v1h-1V4Z"></path>
+          <path d="M9 2h5v5H9V2Zm1 1v3h3V3h-3ZM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8H8Zm2 2H9V9h1v1Zm4 2h-1v1h-2v1h3v-2Zm-4 2v-1H8v1h2Z"></path>
+          <path d="M12 9h2V8h-2v1Z"></path>
+        </svg>
+      ),
+      href: "scan_qr",
+    },
+  ];
+  if (user?.is_admin)
+    NAV_MENU = [
+      ...NAV_MENU,
+      {
+        name: "Almighty",
+        icon: UserIcon,
+        href: "admin",
+      },
+    ];
   if (!events) return;
   return (
     <Clp open={open}>
@@ -52,13 +79,7 @@ const Collapse = ({ open }: { open: boolean }) => {
         <Button className="mb-6" placeholder="" key="conttestantButton">
           {events ? events[0].event_name : "no event"}
         </Button>
-        {user?.is_admin && (
-          <a href="/admin">
-            <Button className="mb-6" placeholder="" key="conttestantButton">
-              Admin
-            </Button>
-          </a>
-        )}
+
         <Typography
           placeholder={""}
           variant="paragraph"
@@ -66,7 +87,7 @@ const Collapse = ({ open }: { open: boolean }) => {
         >
           Votes Left : {(votesLeft && votesLeft.length) || 0}{" "}
           <span className="hidden md:block capitalize">
-            ({votesLeft?.join(", ")})
+            {votesLeft && votesLeft.length > 0 && "=>" + votesLeft?.join(", ")}
           </span>
         </Typography>
         <Progress
